a5a96d0309388efb11d4f018f34d1171995cb4c5
Piyush Prajapati <piyushprajapati7120@gmail.com>
2026-06-02 20:47:07 +0530

    MAP Fixes

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(
   helmet({
